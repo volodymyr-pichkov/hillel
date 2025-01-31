840e1e02867da379b80a65e28d51f53b2eeab1b2
Volodymyr Pichkov <volodymyr2000pichkov@gmail.com>
2025-01-31 15:10:04 +0200

    footer fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,42 +3,52 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="w-full h-auto max-w-[1366px] max-h-[224px] flex items-center bg-layout-white">
-      <div className="flex flex-col justify-between items-center">
-        <div className="flex items-center">
+
+      <div className="flex flex-col items-center">
+
+        <div className="flex">
           <img
-            className="mt-[53px] mr-[11px] mb-[18px] ml-[101px]"
+            className="#"
             src="../assets/images/logo.svg"
             alt="logo"
           />
-          <h2 className="flex flex-col text-title-dark font-work text-xl not-italic leading-normal font-bold justify-center w-[119px] h-[14px] mt-[60px] mr-[136px] mb-[20px]">
+          <h2 className="flex flex-col text-title-dark font-work text-xl leading-normal font-bold justify-center">
             Чіп Чендж
           </h2>
         </div>
-        <div className="flex flex-col">
+
+        <div className="flex flex-col justify-center text-nav-text font-roboto text-xs leading-[1.4]">
           <p>04128, м.Київ, вул. Хрещатик, 19</p>
           <p>Ліцензія НБУ №156</p>
           <p>&#169; ПАТ ЧіпЧендж, 2019-2023</p>
         </div>
+
       </div>
 
-      {/*
-      <nav className="flex flex-col justify-between items-center font-roboto text-nav-text not-italic font-normal leading-normal">
+      
+      <nav className="flex flex-col justify-center font-roboto text-nav-text leading-normal space-y-5">
+
         <ul>
-            <li><a href="#">Послуги</a></li>
-            <li><a href="#">Конвертер валют</a></li>
-            <li><a href="#">Контакти</a></li>
-            <li><a href="#">Задати питання</a></li>
+            <li className="#"><a href="#" >Послуги</a></li>
+            <li className="#"><a href="#" >Конвертер валют</a></li>
+            <li className="#"><a href="#" >Контакти</a></li>
+            <li className="#"><a href="#" >Задати питання</a></li>
         </ul>
+
       </nav>
 
       <div className="flex flex-col">
+
         <div className="flex justify-center">
         <img src="../assets/icons/phone.svg" alt="phone icon" className="#" />
-        <p className="#">3773</p>
+        <p className="flex flex-col justify-center font-roboto text-base font-medium leading-[1.4] text-title-dark">3773</p>
         </div>
-        <p className="#">Цілодобова підтримка</p>
-      </div>
 
+        <p className="flex flex-col justify-center text-nav-text text-xs leading-[1.2]">Цілодобова підтримка</p>
+
+      </div>
+      
+{/*
       <div className="flex flex-col">
         <div className="flex justify-center">
         <img src="../assets/icons/handset.svg" alt="phone icon" className="#" />
@@ -46,8 +56,8 @@ const Footer = () => {
         </div>
         <p className="#">Безкожтовно для дзвінків в межах України</p>
       </div>
+*/}
       
-      */}
       
     </footer>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,10 +9,10 @@ const Header = () => {
           src="../assets/images/logo.svg"
           alt="logo"
         />
-        <h1 className="flex flex-col text-title-dark font-work text-xl not-italic leading-normal font-bold justify-center w-[112px] h-[14px] mt-[39px] mr-[80px] mb-[38px]">Чіп Чендж</h1>
+        <h1 className="flex flex-col text-title-dark font-work text-xl  leading-normal font-bold justify-center w-[112px] h-[14px] mt-[39px] mr-[80px] mb-[38px]">Чіп Чендж</h1>
       </div>
       <nav>
-        <ul className="flex justify-between items-center font-roboto text-nav-text not-italic font-normal leading-normal">
+        <ul className="flex justify-between items-center font-roboto text-nav-text leading-normal">
           <li className="flex flex-col justify-center w-[71px] h-[11px] mt-[42px] mr-[38px] mb-[38px]"><a href="#">Послуги</a></li>
           <li className="flex flex-col justify-center w-[174px] h-[11px] mt-[42px] mr-[10px] mb-[38px]"><a href="#" className="hover:text-link">Конвертер валют</a></li>
           <li className="flex flex-col justify-center w-[74px] h-[11px] mt-[42px] mr-[52px] mb-[38px]"><a href="#">Контакти</a></li>
@@ -21,7 +21,7 @@ const Header = () => {
       </nav>
       <div className="flex items-center">
       <a href="#"><img className="mt-[36px] mr-[15px] mb-[33px]" src="../assets/icons/personal-account.svg" alt="personal account" /> </a>
-      <a href="#" className="flex items-center font-roboto non-italic font-normal leading-normal w-[144px] h-[11px] mt-[42px] mr-[87px] mb-[38px]">Особистий кабінет</a>
+      <a href="#" className="flex items-center font-roboto leading-normal w-[144px] h-[11px] mt-[42px] mr-[87px] mb-[38px]">Особистий кабінет</a>
       </div>
     </header>
   );
