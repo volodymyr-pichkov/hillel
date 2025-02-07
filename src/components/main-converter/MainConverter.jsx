768f6cefb6b8dd2edf7f91f83a78fbfc8a11b271
Volodymyr Pichkov <volodymyr2000pichkov@gmail.com>
2025-02-07 13:18:30 +0200

    pzdc

--- src/components/main-converter/MainConverter.jsx
+++ src/components/main-converter/MainConverter.jsx
@@ -15,24 +15,30 @@ const MainConverter = () => {
   return (
     <main className="bg-background py-20 px-52">
       <div className="rounded bg-white min-h-[400px] py-14">
-      <h3 className="mr-[434px] ml-[48px] text-title text-4xl font-bold leading-[1.4]">Конвертер валют</h3>
-      <div>
-      <p className="mt-[70px] ml-[65px] mr-[258px] mb-[46px] text-navigation font-roboto text-xl font-medium leading-[1.4]">В мене є</p>
-      <form>
-        <input type="number" className="фзborder border-calendar"/>
-
-        <SelectedCurrency
-          selectedCurrency={selectedCurrency}
-          setSelectedCurrency={setSelectedCurrency}
-        />
-        <DateInput
-          date={date}
-          toggleCalendar={toggleCalendar}
-          setDate={setDate}
-          calendarOpen={calendarOpen}
-        />
-      </form>
-      </div>
+        <h3 className="mr-[434px] ml-[48px] text-title text-4xl font-bold leading-[1.4]">
+          Конвертер валют
+        </h3>
+        <div>
+          <p className="mt-[70px] ml-[65px] mr-[258px] mb-[30px] text-navigation font-roboto text-xl font-medium leading-[1.4]">
+            В мене є
+          </p>
+          <form>
+            <input
+              type="number"
+              className="ml-[65px] mr-[15px] border border-calendar w-[220px]"
+            />
+            <SelectedCurrency
+              selectedCurrency={selectedCurrency}
+              setSelectedCurrency={setSelectedCurrency}
+            />
+            <DateInput
+              date={date}
+              toggleCalendar={toggleCalendar}
+              setDate={setDate}
+              calendarOpen={calendarOpen}
+            />
+          </form>
+        </div>
       </div>
     </main>
   );
--- src/components/main-converter/components/DateInput.jsx
+++ src/components/main-converter/components/DateInput.jsx
@@ -4,23 +4,28 @@ import "react-datepicker/dist/react-datepicker.css";
 
 const DateInput = ({ date, setDate, toggleCalendar, calendarOpen }) => {
   return (
-    <div>
-      <input
-        type="text"
-        value={date ? date.toLocaleDateString() : ""}
-        readOnly
+    <div className="flex items-center mt-6 ml-[65px] mb-[55px] mr-[389px] relative w-[220px]">
+<input
+  type="text"
+  value={date ? date.toLocaleDateString() : ""}
+  readOnly
+  onClick={toggleCalendar}
+  className="rounded border border-calendar text-navigation w-full py-[18px] pl-[20px]"
+/>
+      <FaRegCalendarAlt
         onClick={toggleCalendar}
+        className="cursor-pointer absolute right-0 mr-[17px]"
       />
 
-      <FaRegCalendarAlt onClick={toggleCalendar} />
-
       {calendarOpen && (
-        <DatePicker
-          selected={date}
-          onChange={setDate}
-          inline
-          onClickOutside={toggleCalendar}
-        />
+        <div className="absolute w-full top-[calc(100%+10px)]">
+          <DatePicker
+            selected={date}
+            onChange={setDate}
+            inline
+            onClickOutside={toggleCalendar}
+          />
+        </div>
       )}
     </div>
   );
