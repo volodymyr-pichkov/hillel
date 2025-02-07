23d3325d829294d4e8e2931298e9b8b4fadccd13
Volodymyr Pichkov <volodymyr2000pichkov@gmail.com>
2025-02-07 13:37:06 +0200

    pzdc trilogy

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -345,6 +345,7 @@ module.exports = function (webpackEnv) {
           exclude: /@babel(?:\/|\\{1,2})runtime/,
           test: /\.(js|mjs|jsx|ts|tsx|css)$/,
           loader: require.resolve('source-map-loader'),
+          exclude: /node_modules\/react-datepicker/,
         },
         {
           // "oneOf" will traverse all following loaders until one will
